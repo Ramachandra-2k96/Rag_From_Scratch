e61a1fe92ab16cae0d66a52ccf7ec48bad84a8d6
Ramachandra-2k96 <ramachandraudupa.sangamone@gmail.com>
2024-08-27 08:55:12 +0530

    first commit

---
+++ ollama_use.py
@@ -0,0 +1,15 @@
+from langchain_community.chat_models import ChatOllama
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+
+llm = ChatOllama(
+    model = "hermes3",
+    keep_alive = -1,
+    temparature = 0.7,
+    max_tokens = 2048)
+prompt  = ChatPromptTemplate.from_template("Write a 100 letter essay for {topic} from the perspective of a {profession}. ")
+
+chain = prompt | llm | StrOutputParser()
+stream = chain.stream({"topic":"LLMs","profession":"shipping magnate"})
+for c in stream:
+    print(c,flush=True,end="")
